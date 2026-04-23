e5d82872fef82da32b69d128a7ec27050c8089aa
nixondev <cnixon@lokion.com>
2026-04-22 21:29:25 -0500

    game 1

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import path from 'path';
 
 export default defineConfig({
+  base: '/phaser3/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
